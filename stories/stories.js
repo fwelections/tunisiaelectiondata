a621a21d7300868121f53fa5b04a94444178d368
radproject <radhouanef@gmail.com>
2014-12-28 01:03:53 +0100

    story  controller and further refactoring

--- stories/stories.js
+++ stories/stories.js
@@ -10,17 +10,34 @@ angular.module('ted.stories', ['ngRoute'])
     .when('/stories/:election', {
     templateUrl: 'stories/stories.html',
     controller: 'storiesCtrl'
+  })
+  .when('/stories/:election/:map', {
+    templateUrl: 'stories/story.html',
+    controller: 'storyCtrl'
   });
 }])
 
-.controller('storiesCtrl', ['Maps','$scope','$routeParams', function(Maps,$scope,$routeParams) {
-    console.log($routeParams);
+.controller('storiesCtrl', ['Stories','$scope','$routeParams', function(Stories,$scope,$routeParams) {
     if($routeParams.election == undefined)
         // default stories are  presidential elections ones ; 
       $routeParams.election= 'pre';
-    var promise = Maps.listAll($routeParams.election,'en');
-    promise.then(function(response) {        
+    var promise = Stories.listAll($routeParams.election,'en');
+    promise.then(function(response) {  
+        $scope.elec =  $routeParams.election;
         $scope.election = response.data;
+    });
+
+}])
+.controller('storyCtrl', ['Stories','$scope','$routeParams', function(Stories,$scope,$routeParams) {
+     if($routeParams.election == undefined)
+        // default stories are  presidential elections ones ; 
+      $routeParams.election= 'pre';
+    var promise = Stories.getStory($routeParams.election,$routeParams.map,'en');
+    promise.then(function(response) {        
+        if(response.data.story == "{}")
+            console.log('story not available ')
+            else
+                console.log(response.data.story);
     });
 
 }]);
